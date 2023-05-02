58722428ae011995314eb5faedc8824417602b79
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-05-02 18:35:03 +0600

    person and color added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 333;
+
+const person1 = { name: 'mahfuzur', profession: 'singer' };
+const person2 = { name: 'keka afa', profession: 'cook' };
+
+const personstyle = {
+  color: 'blue',
+  backgroundColor: 'yellow',
+  borderRadius: '10px',
+  padding: '5px'
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>show number:{number}</p>
+        <div style={personstyle}>
+          <h2>singer section</h2>
+          <p>performer:{person1.name} {person1.profession}</p>
+          <p>chef:{person2.name} {person2.profession} </p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
